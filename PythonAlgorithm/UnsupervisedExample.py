01fdd00739f19f3342b65393f550367c8684ed76
HONGSEONGCHAN <seongchan_@naver.com>
2021-01-18 20:20:34 +0900

    비지도 학습 예제 (군집화, PCA)

--- PythonAlgorithm/UnsupervisedExample.py
+++ PythonAlgorithm/UnsupervisedExample.py
@@ -74,3 +74,54 @@ for target, color in zip(targets,colors):
                , s = 50)
 ax.legend(targets)
 ax.grid()
+
+## 군집화 예제
+
+# K평균 군집화
+# 주어진 데이터를 K개의 클러스터로 묶은 알고리즘
+# 각 클러스터와 거리 차이의 분산을 최소화하는 방식으로 동작
+# K는 클러스터의 중심 수를 의미
+
+# 사이킷런 라이브러리로 K-평균 군집화 함수 불러오기
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+# iris 데이터셋 불러오기
+iris_dataset = load_iris()
+
+# train 데이터셋 설정
+train_input, train_label, test_input = train_test_split(iris_dataset['data'], iris_dataset['target'], test_size=0.25, random_state=42)
+
+# K 값을 의미하는 군집의 개수 설정
+k_means = KMeans(n_clusters=3)
+
+# train 데이터를 이용해서 모델 학습시키기
+k_means.fit(train_input)
+
+# 클러스터 군집 확인
+# 0번째 군집인지 1번째 혹은 2번째 군집인지 나타내는 라벨
+# 실행될 때마다 결과가 변동될 수 있음
+k_means.labels_
+
+# 각 군집의 종 확인하기
+train_label[k_means.labels_ == 0]
+train_label[k_means.labels_ == 1]
+train_label[k_means.labels_ == 2]
+
+# test 데이터를 이용해서 결과값 (라벨) 예측하기
+predict_cluster = k_means.predict(test_input)
+
+# 군집을 의미하는 값을 iris의 종을 의미하는 값으로 변경하기
+import numpy as np
+
+np_arr = np.array(predict_cluster)
+np_arr[np_arr == 0], np_arr[np_arr == 1], np_arr[np_arr == 2] = 3, 4, 5
+np_arr[np_arr == 3] = 1
+np_arr[np_arr == 4] = 0
+np_arr[np_arr == 5] = 2
+
+predict_label = np_arr.tolist()
+
+# 예측 값과 실제 값을 비교하여 성능 비교
+print(f"test accuracy: {np.mean(predict_label == test_label):.2f}")
